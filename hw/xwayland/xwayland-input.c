1c72a04d3b166980b2290684c2af19e4db0cad9e
Jason Gerecke <killertofu@gmail.com>
2015-12-18 17:18:31 -0800

    xwayland: Connect to tablet_manager and tablet_seat interfaces
    Obtains references to the tablet_manager and gets a tablet_seat for the first
    X seat known.
    
    TODO: Get tablet_seat objects for /all/ seats known to X

--- hw/xwayland/xwayland-input.c
+++ hw/xwayland/xwayland-input.c
@@ -827,15 +827,44 @@ xwl_seat_destroy(struct xwl_seat *xwl_seat)
     free(xwl_seat);
 }
 
+
+static void
+create_tablet_manager(struct xwl_screen *xwl_screen, uint32_t id, uint32_t version)
+{
+    struct xwl_seat *xwl_seat;
+
+    LogMessageVerbSigSafe(X_NOTICE, -1, "xwayland: binding local tablet_manager object to server\n");
+    xwl_screen->tablet_manager = wl_registry_bind(xwl_screen->registry,
+                                                  id,
+                                                  &zwp_tablet_manager_v1_interface,
+                                                  min(version,1));
+
+    // TODO: Have this actually do its work for all the seats :)
+    LogMessageVerbSigSafe(X_NOTICE, -1, "xwayland: requesting tablet seat for active seats\n");
+    xwl_seat = xorg_list_first_entry(&xwl_screen->seat_list, struct xwl_seat, link);
+    xwl_seat->tablet_seat = zwp_tablet_manager_v1_get_tablet_seat(xwl_screen->tablet_manager, xwl_seat->seat);
+
+    LogMessageVerbSigSafe(X_NOTICE, -1, "xwayland: END create_tablet_manager\n");
+}
+
 static void
 input_handler(void *data, struct wl_registry *registry, uint32_t id,
               const char *interface, uint32_t version)
 {
     struct xwl_screen *xwl_screen = data;
 
+    LogMessageVerbSigSafe(X_NOTICE, -1, "xwayland: notified of interface %s (ver %d)\n", interface, version);
+
     if (strcmp(interface, "wl_seat") == 0 && version >= 3) {
         create_input_device(xwl_screen, id, version);
         xwl_screen->expecting_event++;
+    }
+
+    if (strcmp(interface, "zwp_tablet_manager_v1") == 0 && version >= 1) {
+        create_tablet_manager(xwl_screen, id, version);
+
+        //FIXME: Is this needed? What does it do?
+        //xwl_screen->expecting_event++;
     }
 }
 
